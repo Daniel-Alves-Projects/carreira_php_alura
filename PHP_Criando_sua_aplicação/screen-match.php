cc2e56427d82a81a814bb523220b6841edc6059f
Daniel-Alves <assistenciatecnica@ballke.com.br>
2025-11-03 11:47:51 -0300

    variaveis e operadores

--- PHP_Criando_sua_aplicação/screen-match.php
+++ PHP_Criando_sua_aplicação/screen-match.php
@@ -1,2 +1,11 @@
 <?php
-echo "Bem vindo ao Screen Match!";
+echo "Bem vindo ao Screen Match!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+$planoPrime = true;
+
+echo $nomeFilme;
+ ?>
